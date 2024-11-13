69df23aa4a02eee2f048452c9de8ecafd73d8807
Sagan Shrestha <saganshrestha01@gmail.com>
2024-11-13 22:11:36 +0545

    Added Functionality

---
+++ src/assets/data.js
@@ -0,0 +1,43 @@
+
+export const data = [
+    {
+        question: "Which device is required for the Internet Connection?",
+        option1: "Modem",
+        option2: "Router",
+        option3: "LAN Cable",
+        option4: "Pen Drive",
+        answer: 1,
+    },
+    {
+        question: "Which continent has the highest number of countries?",
+        option1: "Asia",
+        option2: "Europe",
+        option3: "North America",
+        option4: "Africa",
+        answer: 4,
+    },
+    {
+        question: "Junk email is also called?",
+        option1: "Spam",
+        option2: "Fake",
+        option3: "Archived",
+        option4: "Bin",
+        answer: 1,
+    },
+    {
+        question: "A computer cannot boot if it does not have the?",
+        option1: "Application Software",
+        option2: "Internet",
+        option3: "Operating System",
+        option4: "Mouse",
+        answer: 3,
+    },
+    {
+        question: "First page of website is termed as?",
+        option1: "Index Page",
+        option2: "Homepage",
+        option3: "Sitemap",
+        option4: "Frontend",
+        answer: 2,
+    },
+]
